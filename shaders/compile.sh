bbd6f0876b715397ea304c9f38ee22dd9c7dd538
katajisto <t@ktj.st>
2025-02-09 21:34:42 +0200

    Work

---
+++ shaders/compile.sh
@@ -0,0 +1,18 @@
+rm -rf SPIRV
+rm -rf MSL
+mkdir SPIRV
+mkdir MSL
+
+for filename in *.vert.slang; do
+    if [ -f "$filename" ]; then
+        slangc -stage vertex -entry mainf -o "./SPIRV/${filename/.slang/.spv}"  "$filename"
+        slangc -stage vertex -entry mainf -o "./MSL/${filename/.slang/.metal}"    "$filename" 
+    fi
+done
+
+for filename in *.frag.slang; do
+    if [ -f "$filename" ]; then
+        slangc -stage fragment -entry mainf -o "./SPIRV/${filename/.slang/.spv}" "$filename"
+        slangc -stage fragment -entry mainf -o "./MSL/${filename/.slang/.metal}" "$filename"
+    fi
+done
